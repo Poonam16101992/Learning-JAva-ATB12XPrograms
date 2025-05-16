e7c08cdfcbb2d22df0f0f51c0123f4841b8fe429
Poonam16101992 <Poonam.pahi.sharma@gmail.com>
2025-05-16 16:23:19 +0530

    16 may 2025

---
+++ src/ex_02_JavaBasics_Part2/Lab024_printf.java
@@ -0,0 +1,14 @@
+package ex_02_JavaBasics_Part2;
+
+public class Lab024_printf {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        System.out.printf("%d + %d",a,b);
+        System.out.println();
+        System.out.printf("a=%d  b=%d",a,b);
+        //%d---> int
+        //%f---> float,double
+        //%s-->string
+        //%b--> boolean
+    }
+}
