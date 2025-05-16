b77e6af6946f43474440351005bb84f6ccb8d3ab
Poonam16101992 <Poonam.pahi.sharma@gmail.com>
2025-05-16 16:30:35 +0530

    16 may 2025

--- src/ex_02_JavaBasics_Part2/Task_table_of_5_using_printf.java
+++ src/ex_02_JavaBasics_Part2/Task_table_of_5_using_printf.java
@@ -3,25 +3,25 @@ package ex_02_JavaBasics_Part2;
 public class Task_table_of_5_using_printf {
     public static void main(String[] args) {
         int num=5;
-        System.out.printf("%d * 1 = 5",num);
+        System.out.printf("%d * 1 = %d",num,num);
         System.out.println();
-        System.out.printf("%d * 2 = 10",num);
+        System.out.printf("%d * 2 = %d",num,num*2);
         System.out.println();
-        System.out.printf("%d * 3 = 15",num);
+        System.out.printf("%d * 3 = %d",num,num*3);
         System.out.println();
-        System.out.printf("%d * 4 = 20",num);
+        System.out.printf("%d * 4 = %d",num,num*4);
         System.out.println();
-        System.out.printf("%d * 5 = 25",num);
+        System.out.printf("%d * 5 = %d",num,num*5);
         System.out.println();
-        System.out.printf("%d * 6 = 30",num);
+        System.out.printf("%d * 6 = %d",num,num*6);
         System.out.println();
-        System.out.printf("%d * 7 = 35",num);
+        System.out.printf("%d * 7 = %d",num,num*7);
         System.out.println();
-        System.out.printf("%d * 8 = 40",num);
+        System.out.printf("%d * 8 = %d",num,num*8);
         System.out.println();
-        System.out.printf("%d * 9 = 45",num);
+        System.out.printf("%d * 9 = %d",num,num*9);
         System.out.println();
-        System.out.printf("%d * 10 =50",num);
+        System.out.printf("%d * 10 =%d",num,num*10);
 
 
     }
